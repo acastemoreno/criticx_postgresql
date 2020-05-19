ca053987850ae517ec45272a4d1b88fed50925d9
Albert <acastemoreno@gmail.com>
2020-05-19 12:01:11 -0500

    Update Game data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Game.create(name: 'Rune Factory 4 Special', genre: 'Role-playing (RPG)', price: 5999, release_date: Date.new(2019, 7, 25))
-Game.create(name: 'Boderlands 3 ', genre: 'Simulator', price: 5999, release_date: Date.new(2019, 9, 13))
+Game.create(name: 'Borderlands 3', genre: 'Simulator', price: 5999, release_date: Date.new(2019, 9, 13))
 
 list_games = [{:name=>"Zombie Driver: Immortal Edition", :genre=>"Racing", :price=>5999, :release_date=>"2019-Jul-25"},
 {:name=>"Remnant: From the Ashes", :genre=>"Shooter", :price=>6999, :release_date=>"2019-Aug-20"},
@@ -39,3 +39,22 @@ p Game.where(price: 5999)
 puts %Q{All games where release year is 2019
 ----------}
 p Game.where('extract(year from release_date) = ?', '2019')
+
+puts %Q{Find game, set columns and save
+----------}
+p game = Game.find_by(name: 'Borderlands 3')
+game.genre = 'Shooter'
+game.save
+p game = Game.find_by(name: 'Borderlands 3')
+
+puts %Q{Find game, Update game
+----------}
+p game = Game.find_by(name: 'Pokémon Shield')
+game.update(price: 3999)
+p game = Game.find_by(name: 'Pokémon Shield')
+
+puts %Q{Find role games, Update all games
+----------}
+p games = Game.where(genre: 'Role-playing (RPG)')
+games.update_all price: 2999
+p games = Game.where(genre: 'Role-playing (RPG)')
