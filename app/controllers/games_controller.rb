433aeb2106f36059aa2dc782beafe2cf97d8381e
Albert <acastemoreno@gmail.com>
2020-05-20 11:34:47 -0500

    Show a specific game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,4 +3,9 @@ class GamesController < ApplicationController
     @games = Game.all   # Get all the games and saved them on @games
     render json: @games # Render all the games on JSON format
   end
+
+  def show
+    @game = Game.find(params[:id]) # Get an specific game using the id of the URL
+    render json: @game
+  end
 end
