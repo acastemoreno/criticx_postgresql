7f248edc90f5650c667e5e31bc94deeaef0872fe
Albert <acastemoreno@gmail.com>
2020-05-20 11:55:52 -0500

    Create a new Game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,4 +8,18 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id]) # Get an specific game using the id of the URL
     render json: @game
   end
+
+  def create
+    @game = Game.new(game_params())
+    if @game.save
+      render json: @game
+    else
+      render json: @game.errors
+    end
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:name, :genre, :price, :release_date)
+  end
 end
