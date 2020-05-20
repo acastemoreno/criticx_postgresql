2bc98939def07f0d8d42189bcfa8ccd63aa8b0df
Albert <acastemoreno@gmail.com>
2020-05-20 14:54:05 -0500

    Task 3 Create a new Company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -9,4 +9,18 @@ class CompaniesController < ApplicationController
     
     render json: @company
   end
+
+  def create
+    @company = Company.new(company_params())
+    if @company.save
+      render json: @company
+    else
+      render json: @company.errors
+    end
+  end
+
+  private
+  def company_params()
+    params.require(:company).permit(:name, :description, :start_date, :country)
+  end
 end
